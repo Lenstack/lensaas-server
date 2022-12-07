2be5a54f776c87ef1bde7ce1e49bf360cd876556
LEONARDO OSPINA <42081538+Lenstack@users.noreply.github.com>
2022-12-06 21:47:32 -0500

    Update Feature

--- web/src/containers/home.feature.tsx
+++ web/src/containers/home.feature.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
+import Dashboard from "@/assets/dashboard.jpg";
+
 export const HomeFeature = () => {
     return (
-        <section className="container mx-auto py-10">
-            <h1 className="text-2xl font-bold">
-                Feature
-            </h1>
+        <section className="container mx-auto py-5">
+            <section className={"flex flex-col gap-10 text-center"}>
+                <h2 className={"text-5xl font-bold text-center"}>How works</h2>
+                <p className={"text-2xl text-gray-700"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam doloremque laudantium modi pariatur reprehenderit!</p>
+            </section>
+            <section className={"p-28"}>
+                <Image src={Dashboard} alt={"logo"} className={" object-fill rounded-2xl"}/>
+            </section>
         </section>
     )
 }
