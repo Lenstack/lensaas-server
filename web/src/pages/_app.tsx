eeb73c85722333be59c759d8081371b73bdb4b32
LEONARDO OSPINA <42081538+Lenstack@users.noreply.github.com>
2022-12-11 18:26:13 -0500

    Update Head From Index

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {ThemeProvider} from "next-themes";
+import Head from "next/head";
 
 export default function App({Component, pageProps}: AppProps) {
     return (
         <ThemeProvider attribute="class">
+            <Head>
+                <meta charSet="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <meta name="description" content="Lensaas With Microservices"/>
+                <meta name="author" content="Lenstack"/>
+                <meta name="keywords" content="keywords"/>
+                <meta name="theme-color" content="#fffff"/>
+                <title>Lensaas</title>
+            </Head>
             <Component {...pageProps} />
         </ThemeProvider>
     )
