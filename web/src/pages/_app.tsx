6489ad78835021107e57fee810f3398bb036bb8f
LEONARDO OSPINA <42081538+Lenstack@users.noreply.github.com>
2022-12-11 18:33:15 -0500

    Add lang tag next_config and update head

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -12,7 +12,8 @@ export default function App({Component, pageProps}: AppProps) {
                 <meta name="description" content="Lensaas With Microservices"/>
                 <meta name="author" content="Lenstack"/>
                 <meta name="keywords" content="keywords"/>
-                <meta name="theme-color" content="#fffff"/>
+                <meta name="theme-color" content="#EEF0F2"/>
+                <link rel="manifest" href="/manifest.json" />
                 <title>Lensaas</title>
             </Head>
             <Component {...pageProps} />
