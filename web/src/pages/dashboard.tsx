f75dca18505ba892c433759555d214bb8bc49ee3
LEONARDO OSPINA <42081538+Lenstack@users.noreply.github.com>
2022-12-11 17:48:59 -0500

    Refactor bg theme

--- web/src/pages/dashboard.tsx
+++ web/src/pages/dashboard.tsx
@@ -2,7 +2,7 @@ import {DashboardAside, DashboardHeader} from "@/containers";
 
 export default function Dashboard() {
     return (
-        <div className={"bg-gradient-to-b from-gray-100 to-gray-300 min-h-screen overflow-hidden"}>
+        <div className={"min-h-screen overflow-hidden"}>
             <DashboardHeader/>
             <DashboardAside/>
         </div>
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,8 +2,7 @@ import {HomeContact, HomeFeature, HomeFooter, HomeHeader, HomeHero, HomePricing}
 
 export default function Home() {
     return (
-        <div className={"min-h-screen overflow-hidden bg-gradient-to-b from-gray-100 to-gray-300" +
-            "dark:bg-gradient-to-br dark:from-zinc-900 dark:via-zinc-900 dark:to-neutral-900"}>
+        <div className={"min-h-screen overflow-hidden"}>
             <HomeHeader/>
             <HomeHero/>
             <HomeFeature/>
