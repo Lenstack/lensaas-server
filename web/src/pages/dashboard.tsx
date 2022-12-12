97f9d03036d631b6ca28d72a784cbe34c03a68f8
LEONARDO OSPINA <42081538+Lenstack@users.noreply.github.com>
2022-12-11 21:45:17 -0500

    Update Styles And Add Content Component

--- web/src/pages/dashboard.tsx
+++ web/src/pages/dashboard.tsx
@@ -1,10 +1,11 @@
-import {DashboardAside, DashboardHeader} from "@/containers";
+import {DashboardAside, DashboardContent, DashboardHeader} from "@/containers";
 
 export default function Dashboard() {
     return (
-        <div className="min-h-screen overflow-hidden">
+        <div className="min-h-screen overflow-hidden grid grid-cols-12 auto-rows-auto gap-5">
             <DashboardHeader/>
             <DashboardAside/>
+            <DashboardContent/>
         </div>
     )
 }
