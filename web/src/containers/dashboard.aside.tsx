df60c25805b87ff6d456455f1724a482b862d524
LEONARDO OSPINA <42081538+Lenstack@users.noreply.github.com>
2022-12-11 21:45:58 -0500

    Update Styles

--- web/src/containers/dashboard.aside.tsx
+++ web/src/containers/dashboard.aside.tsx
@@ -1,16 +1,19 @@
-import {Navigation} from "@/components";
+import {Navigation, ThemeToggle} from "@/components";
 import {DASHBOARD_LINKS} from "@/constants";
 
 export const DashboardAside = () => {
     return (
-        <aside className="bg-amber-100">
-            <Navigation>
-                <Navigation.Container className={"flex flex-col gap-5"}>
+        <aside className="bg-[#F6F6F6] dark:bg-zinc-800 min-h-full col-start-1 col-end-2 row-start-1 row-end-7 p-5">
+            <Navigation className="flex flex-col content-center items-center justify-center gap-10">
+                <Navigation.Container className="flex flex-col items-center gap-5">
                     {
                         DASHBOARD_LINKS.map(({id, title, url}) => (
                             <Navigation.Item key={id} href={url}>{title}</Navigation.Item>
                         ))
                     }
+                </Navigation.Container>
+                <Navigation.Container>
+                    <ThemeToggle/>
                 </Navigation.Container>
             </Navigation>
         </aside>
--- web/src/containers/dashboard.header.tsx
+++ web/src/containers/dashboard.header.tsx
@@ -1,6 +1,6 @@
 export const DashboardHeader = () => {
     return (
-        <header className="">
+        <header className="col-start-3 col-end-12 row-start-1 row-end-2 p-10">
             <h1 className="text-2xl font-bold">
                 Header
             </h1>
