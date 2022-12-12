b66c9e3aff2d38198ef2c32acacedd8d4fbb4d01
LEONARDO OSPINA <42081538+Lenstack@users.noreply.github.com>
2022-12-11 21:44:40 -0500

    Update styles toggle theme

--- web/src/components/theme_toggle.tsx
+++ web/src/components/theme_toggle.tsx
@@ -26,7 +26,7 @@ export const ThemeToggle = () => {
 
     return (
         <div
-            className={"flex items-center bg-[#2C3543] dark:bg-[#E1F2EC] rounded-full align-center p-2 cursor-pointer"}
+            className={"flex items-center justify-center bg-[#2C3543] dark:bg-[#E1F2EC] rounded-full h-10 w-10 cursor-pointer"}
             onClick={toggleTheme}>
             <span>{icon}</span>
         </div>
