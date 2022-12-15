585fc4c1177fc0aded7921b80422a5b631a12258
LEONARDO OSPINA <42081538+Lenstack@users.noreply.github.com>
2022-12-14 22:14:47 -0500

    Add Cert SSL

--- microservices/gateway/infrastructure/graphql.go
+++ microservices/gateway/infrastructure/graphql.go
@@ -3,6 +3,7 @@ package infrastructure
 import (
 	"github.com/go-chi/chi/v5"
 	"go.uber.org/zap"
+	"net"
 	"net/http"
 )
 
@@ -10,8 +11,14 @@ type GraphqlServer struct {
 }
 
 func NewGraphqlServer(port string, router *chi.Mux, logger *zap.Logger) *GraphqlServer {
+	listenServer, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		logger.Sugar().Fatalf("failed to listen: %v", err)
+		return nil
+	}
+
 	logger.Sugar().Info("Graphql Server is running on port: ", port)
-	if err := http.ListenAndServe(":"+port, router); err != nil {
+	if err := http.ServeTLS(listenServer, router, "./cert/server-cert.pem", "./cert/server-key.pem"); err != nil {
 		logger.Sugar().Fatal("Failed to start Graphql Server: ", err)
 		return nil
 	}
