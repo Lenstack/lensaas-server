3d70c78f9c49d95c628bda1828cb985d1d50b9ac
Leonardo Ospina <42081538+Lenstack@users.noreply.github.com>
2023-05-03 18:53:22 -0500

    Update me.go

--- microservices/authentication/internal/core/applications/me.go
+++ microservices/authentication/internal/core/applications/me.go
@@ -9,28 +9,30 @@ import (
 )
 
 func (m *Microservice) Me(wr http.ResponseWriter, req *http.Request) {
+	// Initialize cookie store
 	cookieStore := utils.NewCookieStore()
+	// Get access token from cookie
 	accessToken, err := cookieStore.GetCookieDecrypted(req, cookieStore.AccessTokenCookieName)
 	if err != nil {
 		wr.WriteHeader(http.StatusBadRequest)
 		err = json.NewEncoder(wr).Encode(&models.ErrorResponse{Code: http.StatusBadRequest, Message: "Error getting access token from cookie"})
 		return
 	}
-
+	// Validate access token
 	userId, err := m.Jwt.ValidateToken(accessToken)
 	if err != nil {
 		wr.WriteHeader(http.StatusBadRequest)
 		err = json.NewEncoder(wr).Encode(&models.ErrorResponse{Code: http.StatusBadRequest, Message: "Error validating access token"})
 		return
 	}
-
+	// Get user
 	user, err := m.UserService.Me(userId)
 	if err != nil {
 		wr.WriteHeader(http.StatusBadRequest)
 		err = json.NewEncoder(wr).Encode(&models.ErrorResponse{Code: http.StatusBadRequest, Message: err.Error()})
 		return
 	}
-
+	// Return user in response
 	wr.WriteHeader(http.StatusOK)
 	err = json.NewEncoder(wr).Encode(&models.MeResponse{Code: http.StatusOK, Message: "Successfully retrieved user",
 		User: models.UserMe{
